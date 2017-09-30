efd02d80b8955f330badca983aa2374a8a8317eb
Craig J Smith <nctiggy@gmail.com>
2017-09-30 12:23:57 -0700

    update script and playbook

--- .setup.sh
+++ .setup.sh
@@ -3,7 +3,7 @@ if [ ! -f /usr/local/bin/brew ]; then
   yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-[ -f /usr/local/bin/python ] || brew install python
-[ -f /usr/local/bin/ansible ] ||brew install ansible
+[ -f /usr/local/bin/python2 ] || brew install python
+[ -f /usr/local/bin/ansible ] || brew install ansible
 
 ansible-playbook -i "localhost," -c local .setup-playbook.yml
